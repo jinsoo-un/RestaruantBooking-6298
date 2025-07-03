41cb7a76f0fb083c676f9bc3317d19cb35e111ea
see.jin <bobstar0@naver.com>
2025-07-03 13:19:34 +0900

    step10

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -12,6 +12,7 @@ protected:
 		ON_THE_HOUR = getTime(2021, 3, 26, 9, 0);
 
 		bookingScheduler.setSmsSender(&testableSmsSender);
+		bookingScheduler.setMailSender(&testableMailSender);
 	}
 
 public:
@@ -30,11 +31,14 @@ public:
 	tm NOT_ON_THE_HOUR;
 	tm ON_THE_HOUR;
 	Customer CUSTOMER{ "Fake name", "010-1234-5678" };
+	Customer CUSTOMER_WITH_MAIL{ "Fake name", "010-1234-5678", "test@test.com" };
+
 	const int UNDER_CAPACITY = 1;
 	const int CAPACITY_PER_HOUR = 3;
 
 	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
 	TestableSmsSender testableSmsSender;
+	TestableMailSender testableMailSender;
 };
 
 TEST_F(BookingItem, 예약은정시에만가능하다정시가아닌경우예약불가) {
@@ -104,9 +108,7 @@ TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
 
 TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
 	//arrange
-	TestableMailSender testableMailSender;
 	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
-	bookingScheduler.setMailSender(&testableMailSender);
 
 	//act
 	bookingScheduler.addSchedule(schedule);
@@ -117,10 +119,7 @@ TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
 
 TEST_F(BookingItem, 이메일이있는경우에는이메일발송) {
 	//arrange
-	Customer customerWithEmail{ "Fake name", "010-1234-5678", "test@test.com" };
-	TestableMailSender testableMailSender;
-	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, customerWithEmail };
-	bookingScheduler.setMailSender(&testableMailSender);
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
 
 	//act
 	bookingScheduler.addSchedule(schedule);
