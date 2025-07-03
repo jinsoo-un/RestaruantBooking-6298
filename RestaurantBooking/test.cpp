9ae002d0be20da323c8883843047950560dd62a6
see.jin <bobstar0@naver.com>
2025-07-03 13:11:01 +0900

    step6

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -1,5 +1,6 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
+#include "testable_sms_sender.cpp"
 
 using namespace testing;
 
@@ -87,7 +88,16 @@ TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케
 }
 
 TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
+	//arrange
+	TestableSmsSender testableSmsSender;
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+	bookingScheduler.setSmsSender(&testableSmsSender);
 
+	//act
+	bookingScheduler.addSchedule(schedule);
+
+	//assert
+	EXPECT_EQ(true, testableSmsSender.isSendMethodCalled());
 }
 
 TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
