1e325860e3b91c2d0acd566d9782b5cf4aff37de
see.jin <bobstar0@naver.com>
2025-07-03 12:57:05 +0900

    step3

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -1,19 +1,34 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
+using namespace testing;
+
+class BookingItem : public Test {
+protected:
+	void SetUp() override {
+		NOT_ON_THE_HOUR = getTime(2021, 3, 26, 9, 5);
+		ON_THE_HOUR = getTime(2021, 3, 26, 9, 0);
+	}
+
+public:
+	tm getTime(int year, int mon, int day, int hour, int min) {
+		tm result = { 0, min, hour, day, mon - 1, year - 1900, 0, 0, -1 };
+		mktime(&result);
+		return result;
+	}
+
+	tm NOT_ON_THE_HOUR;
+	tm ON_THE_HOUR;
+	Customer CUSTOMER{ "Fake name", "010-1234-5678" };
+	const int UNDER_CAPACITY = 1;
+	const int CAPACITY_PER_HOUR = 3;
+
+	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
+};
+
+TEST_F(BookingItem, 예약은정시에만가능하다정시가아닌경우예약불가) {
 	//arrange
-	tm notOnTheHour = { 0 };
-	notOnTheHour.tm_year = 2021 - 1900;		// tm 구조체는 1900 년도부터 데이터 기록
-	notOnTheHour.tm_mon = 03 - 1;			// tm 구조체는 month 를 0 부터 관리
-	notOnTheHour.tm_mday = 26;
-	notOnTheHour.tm_hour = 9;				// 9 시
-	notOnTheHour.tm_min = 5;				// 5 분 , 정각이 아님
-	notOnTheHour.tm_isdst = -1;				// 1 : daylight 를 자동 설정으로 맡김
-	mktime(&notOnTheHour);					// 나머지 tm 구조체 맴버 값에 대해 자동으로 계산하여 채움
-	Customer customer{ "Fake name", "010-1234-5678" };
-	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
-	BookingScheduler bookingScheduler{ 3 };
+	Schedule* schedule = new Schedule{ NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
 	//act
 	EXPECT_THROW({
@@ -24,20 +39,9 @@ TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
 	//expected runtime exception
 }
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
+TEST_F(BookingItem, 예약은정시에만가능하다정시인경우예약가능) {
 	//arrange
-	tm onTheHour = { 0 };
-	onTheHour.tm_year = 2021 - 1900;
-	onTheHour.tm_mon = 03 - 1;
-	onTheHour.tm_mday = 26;
-	onTheHour.tm_hour = 9;
-	onTheHour.tm_min = 0;	// 정각
-	onTheHour.tm_isdst = 1;
-	mktime(&onTheHour);
-
-	Customer customer{ "Fake name", "010-1234-5678" };
-	Schedule* schedule = new Schedule{ onTheHour, 1, customer };
-	BookingScheduler bookingScheduler{ 3 };
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
 	//act
 	bookingScheduler.addSchedule(schedule);
@@ -45,31 +49,31 @@ TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
 	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
 }
 
-TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
+TEST_F(BookingItem, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
 
 }
 
-TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케쥴추가성공) {
+TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케쥴추가성공) {
 
 }
 
-TEST(BookingSchedulerTest, 예약완료시SMS는무조건발송) {
+TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
 
 }
 
-TEST(BookingSchedulerTest, 이메일이없는경우에는이메일미발송) {
+TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
 
 }
 
-TEST(BookingSchedulerTest, 이메일이있는경우에는이메일발송) {
+TEST_F(BookingItem, 이메일이있는경우에는이메일발송) {
 
 }
 
-TEST(BookingSchedulerTest, 현재날짜가일요일인경우예약불가예외처리) {
+TEST_F(BookingItem, 현재날짜가일요일인경우예약불가예외처리) {
 
 }
 
-TEST(BookingSchedulerTest, 현재날짜가일요일이아닌경우예약가능) {
+TEST_F(BookingItem, 현재날짜가일요일이아닌경우예약가능) {
 
 }
 
