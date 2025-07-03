9b7c2d02ef15b2702fd7914e74a75f96e462ca00
see.jin <bobstar0@naver.com>
2025-07-03 13:44:29 +0900

    add files

---
+++ RestaurantBooking/testable_mail_sender.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "mail_sender.cpp"
+
+class TestableMailSender : public MailSender {
+public:
+	void sendMail(Schedule* schedule) override {
+		countSendMailMethodIsCalled++;
+	}
+
+	int getCountSendMailMethodIsCalled() {
+		return countSendMailMethodIsCalled;
+	}
+
+private:
+	bool countSendMailMethodIsCalled = 0;
+};
