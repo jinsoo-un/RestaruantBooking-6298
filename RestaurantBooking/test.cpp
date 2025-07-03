b772835a3336def09ec467ea7ac647411dc49def
see.jin <bobstar0@naver.com>
2025-07-03 12:46:04 +0900

    step1

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -2,7 +2,26 @@
 #include "booking_scheduler.cpp"
 
 TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
+	//arrange
+	tm notOnTheHour = { 0 };
+	notOnTheHour.tm_year = 2021 - 1900;		// tm 구조체는 1900 년도부터 데이터 기록
+	notOnTheHour.tm_mon = 03 - 1;			// tm 구조체는 month 를 0 부터 관리
+	notOnTheHour.tm_mday = 26;
+	notOnTheHour.tm_hour = 9;				// 9 시
+	notOnTheHour.tm_min = 5;				// 5 분 , 정각이 아님
+	notOnTheHour.tm_isdst = -1;				// 1 : daylight 를 자동 설정으로 맡김
+	mktime(&notOnTheHour);					// 나머지 tm 구조체 맴버 값에 대해 자동으로 계산하여 채움
+	Customer customer{ "Fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
 
+	//act
+	EXPECT_THROW({
+		bookingScheduler.addSchedule(schedule);
+		}, std::runtime_error);
+
+	//assert
+	//expected runtime exception
 }
 
 TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
